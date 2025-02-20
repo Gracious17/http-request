2e8497fbc26002f48460fb33c6d2cadbb5831a9a
Gracious-kingsley <Kingsleygracious16@gmail.com>
2025-02-21 00:14:25 +0100

    made changes

--- src/component/UserCard.tsx
+++ src/component/UserCard.tsx
@@ -11,7 +11,7 @@ type ClickId = {
 
 const UserCard = ({ name, username, email, phone, website }: ClickId) => {
   return (
-    <div className="text-center items-center justify-center shadow-xl shadow-blue-400 flex flex-col gap-4 mt-5 space-y-4 ">
+    <div className="text-center items-center justify-center shadow-xl shadow-blue-400 flex flex-col gap-4 mt-5 space-y-4  pl-10">
       <h1 className="">Name: {name}</h1>
       <h3 className="text-red-500">Username :{username}</h3>
 
