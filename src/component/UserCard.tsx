bbac0c5c8cc77a1d45d29a335d359e1b23a5613d
Gracious-kingsley <Kingsleygracious16@gmail.com>
2025-02-20 23:04:24 +0100

    made some changes

--- src/component/UserCard.tsx
+++ src/component/UserCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "./Button";
+// import Button from "./Button";
 type ClickId = {
   id: number;
 
@@ -9,7 +9,7 @@ type ClickId = {
   phone: string;
 };
 
-const UserCard = ({ name, username, email, phone, id }: ClickId) => {
+const UserCard = ({ name, username, email, phone }: ClickId) => {
   return (
     <div className="text-center items-center justify-center shadow-xl shadow-blue-400 flex flex-col gap-4 mt-4">
       <h1 className="font-bold text-2xl text-red-400">{name}</h1>
@@ -17,7 +17,7 @@ const UserCard = ({ name, username, email, phone, id }: ClickId) => {
 
       <p>{email}</p>
       <p>{phone}</p>
-      <Button id={id} />
+      {/* <Button id={id} /> */}
     </div>
   );
 };
